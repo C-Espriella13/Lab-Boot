e33e52b15fa67bbbd6133297800d151f56c1877a
C-Espriella13 <cahucaes05@gmail.com>
2025-10-09 18:54:29 -0600

    Deploy Daemon

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,21 @@
 
 cd /home/loscra13/Desktop/Lab-Boot
 
-# fetch remote
+# Fetch from remote
 git fetch origin
 
 CURR=$(git rev-parse Deploy)
 REPO=$(git rev-parse origin/Deploy)
 
 if [ "$CURR" != "$REPO" ]; then
-	echo "Updates Found. Pulling Deploy"
-	git pull origin Deploy
-	npm install
+    echo "[INFO] Updates Found. Pulling Deploy"
+    git pull origin Deploy
+    npm install
+    
+    # Restart the Node service
+    sudo systemctl restart hello.service
+    
+    echo "[INFO] Node app restarted with updates"
 else
-	echo "No updates Found."
+    echo "[INFO] No updates Found."
 fi
-
-# Refresh
-exec /usr/bin/node app.js
